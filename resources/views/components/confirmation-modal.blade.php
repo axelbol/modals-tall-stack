0affddcf348d2ed975120bf90eba2285d440431f
axelbol <axel_lp_alvarado@hotmail.com>
2024-12-11 00:24:42 -0400

    calling from welcome blade

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -4,17 +4,25 @@
     <div class="bg-white shadow-md p-4 max-w-sm h-48 m-auto rounded-md fixed inset-0">
         <div class="flex flex-col h-full justify-between">
             <header>
-                <h3 class="font-bold text-lg">Are you sure?</h3>
+                <h3 class="font-bold text-lg">
+                    {{ $title }}
+                </h3>
             </header>
 
             <main>
-                <p>If you proceed, the entire work will be deleted.</p>
+                <p>
+                    {{ $body }}
+                </p>
             </main>
 
             <footer class="space-x-4">
                 {{-- <button class="bg-gray-400 hover:bg-gray-500 text-xs uppercase py-2 px-4 rounded-md text-white transition-all duration-200">Cancel</button> --}}
-                <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>
-                <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button>
+
+                {{-- call this from welcome blade --}}
+                {{-- <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>
+                <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button> --}}
+
+                {{ $footer }}
             </footer>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,6 +38,17 @@
 
     {{-- Modal --}}
     <x-confirmation-modal>
+        <x-slot name="title">
+            Are you sure?
+        </x-slot>
 
+        <x-slot name="body">
+            If you proceed, the entire work will be deleted.
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>
+            <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button>
+        </x-slot>
     </x-confirmation-modal>
 </x-layout>
