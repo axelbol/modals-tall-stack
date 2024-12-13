e5060c77dadb90052411d632cd49396070de8be7
axelbol <axel_lp_alvarado@hotmail.com>
2024-12-13 00:06:58 -0400

    change welcome & layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,36 @@
 
 <body class="bg-gray-200">
 
-    {{ $slot }}
+    <div class="container max-w-lg mx-auto bg-gray-300">
+        <header class="bg-blue-600 p-4">
+            <h1 class="font-bold text-white">
+                My Site
+            </h1>
+        </header>
+
+        <div class="grid grid-cols-12 p-4">
+            <aside class="mr-6 col-span-3 text-sm">
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                </ul>
+            </aside>
+
+            <main class="text-sm col-span-9">
+
+                {{ $slot }}
+
+            </main>
+        </div>
+
+        <footer class="bg-blue-600 p-4 flex items-center justify-between text-xs text-white">
+            <h5 class="text-xs">My Site</h5>
+            <p>2024</p>
+        </footer>
+    </div>
+
+    {{-- {{ $slot }} --}}
 
     @livewireScripts
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,25 @@
 <x-layout>
-    <div class="container max-w-lg mx-auto bg-gray-300">
-        <header class="bg-blue-600 p-4">
-            <h1 class="font-bold text-white">
-                My Site
-            </h1>
-        </header>
 
-        <div class="grid grid-cols-12 p-4">
-            <aside class="mr-6 col-span-3 text-sm">
-                <ul>
-                    <li>Link 1</li>
-                    <li>Link 2</li>
-                    <li>Link 3</li>
-                </ul>
-            </aside>
-
-            <main class="text-sm col-span-9">
-                <p class="mb-6">
-                    Would you like to delete your account?
-                </p>
-                <p>
-                    <a href="#" class="font-semibold">Yes, delete</a>
-                </p>
-            </main>
-        </div>
-
-        <footer class="bg-blue-600 p-4 flex items-center justify-between text-xs text-white">
-            <h5 class="text-xs">My Site</h5>
-            <p>2024</p>
-        </footer>
-    </div>
+    <p class="mb-6">
+        Would you like to delete your account?
+    </p>
+    <form
+        id="delete-user-form"
+        method="POST"
+        action="/"
+        x-data
+        @submit.prevent="
+            location.hash = '#user-delete-modal';
+        "
+    >
+        @csrf
+        <p>
+            <x-button class="bg-yellow-400 hover:bg-yellow-500">Yes, delete</x-button>
+        </p>
+    </form>
 
     {{-- Modal --}}
-    <x-confirmation-modal>
+    <x-confirmation-modal name="user-delete-modal">
         <x-slot name="title">
             Are you sure?
         </x-slot>
@@ -43,7 +30,7 @@
 
         <x-slot name="footer">
             <x-button class="bg-gray-400 hover:bg-gray-500">Cancel</x-button>
-            <x-button class="bg-blue-400 hover:bg-blue-500">Continue</x-button>
+            <x-button class="bg-blue-400 hover:bg-blue-500" @click="document.querySelector('#delete-user-form').submit()">Continue</x-button>
         </x-slot>
     </x-confirmation-modal>
 </x-layout>
